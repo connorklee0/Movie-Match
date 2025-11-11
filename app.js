3feb52a384729193cb6920048e01bfa5ddc1586d
connorklee0 <connorklee0@gmail.com>
2025-11-11 14:32:58 -0600

    add sort

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Key: b77fdfec
 const search = localStorage.getItem("search")
+const filter = localStorage.getItem("filter")
 const loadText = document.querySelector(".results__load--text")
 const loadSpinner = document.querySelector(".results__loading--spinner")
 const moviesList = document.querySelector(".movies__list")
@@ -21,14 +22,19 @@ searchBar.addEventListener('submit', (event) => {
 })
 
 async function renderMovies(search) {
-    if (!search) {
+    const storedSearch = localStorage.getItem('search') || search;
+    const storedFilter = localStorage.getItem('filter') || 'DEFAULT';
+
+
+    if (!storedSearch) {
         loadText.classList.remove("hidden");
     }
-    searchInput.value = search;
+    searchInput.value = storedSearch;
     resultsHeader.innerHTML = `<span class="highlight">"${search}"</span>`;
-
+    loadSpinner.classList.remove("hidden");
+    
     try {
-        loadSpinner.classList.remove("hidden");
+        
         const response = await fetch(`http://www.omdbapi.com/?apikey=b77fdfec&s=${search}`);
         const moviesData = await response.json();
 
@@ -42,12 +48,41 @@ async function renderMovies(search) {
         loadText.classList.add("hidden");
 
         const firstSixMovies = moviesData.Search.slice(0, 6);
+        const sortedMovies = sortMovies(firstSixMovies, storedFilter)
         moviesList.innerHTML = firstSixMovies.map(movie => movieHTML(movie)).join("");
 
     } catch (error) {
         moviesList.innerHTML = `<div class="highlight results__load--text">Failed to load movies. Try again.</div>`;
         loadSpinner.classList.add("hidden");
     }
+}
+
+function sortMovies(movies, filter) {
+    if (filter === 'LOW_TO_HIGH') {
+        return movies.sort((a, b) => {
+            const yearA = parseInt(a.Year.match(/\d{4}/)?.[0] || '0');
+            const yearB = parseInt(b.Year.match(/\d{4}/)?.[0] || '0');
+            return yearA - yearB;
+        });
+    }
+    else if (filter === 'HIGH_TO_LOW') {
+        return movies.sort((a, b) => {
+            const yearA = parseInt(a.Year.match(/\d{4}/)?.[0] || '0');
+            const yearB = parseInt(b.Year.match(/\d{4}/)?.[0] || '0');
+            return yearB - yearA;
+        });
+    }
+    else {
+        return movies;
+    }
+}
+
+function filterMovies(event) {
+    const filter = event.target.value;
+    const storedSearch = localStorage.getItem('search'); // Get fresh search value
+    
+    localStorage.setItem('filter', filter);
+    renderMovies(storedSearch);
 }
 
 function movieHTML(movie) {
@@ -64,6 +99,8 @@ function movieHTML(movie) {
             </div>`
 }
 
-if (resultsHeader) {
-    renderMovies(search)
-}
+setTimeout(() => {
+    if (resultsHeader) {
+        renderMovies(search)
+    }
+});
